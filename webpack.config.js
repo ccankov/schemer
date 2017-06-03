bc140f88fa147ea531d7cb1828677f0b6ff5224b
Nikita Shalimov <nshalimov@berkeley.edu>
2017-06-02 19:05:00 -0700

    Use vue init to setup file structure

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,48 @@
-var path = require('path');
+var path = require("path");
+var webpack = require("webpack");
+
+var plugins = []; // if using any plugins for both dev and production
+var devPlugins = []; // if using any plugins for development
+
+var prodPlugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      'NODE_ENV': JSON.stringify('production')
+    }
+  }),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: true
+    }
+  })
+];
+
+plugins = plugins.concat(
+  process.env.NODE_ENV === 'production' ? prodPlugins : devPlugins
+);
 
 module.exports = {
   context: __dirname,
-  entry: "./frontend/pathfinder",
+  entry: "./frontend/simply_note.jsx",
   output: {
-    filename: "./assets/bundle.js"
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
+    filename: "bundle.js"
   },
-  devtool: 'source-maps',
-  resolve: {
-    extensions: ["*", ".js"]
-  },
+  plugins: plugins,
   module: {
-   loaders: [
-     {
-       test: [/\.js?$/],
-       exclude: /node_modules/,
-       loader: 'babel-loader',
-       query: {
-         presets: ['es2015']
-       }
-     }
-   ]
- }
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  }
 };
