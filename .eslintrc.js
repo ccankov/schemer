89d81fcabfd1f371263a36792e46dde4f5819947
Nikita Shalimov <ndshalimov@gmail.com>
2017-06-05 11:28:00 -0700

    Update style in Home, App

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,17 +24,11 @@ module.exports = {
   },
   // add your custom rules here
   'rules': {
-    // don't require .vue extension when importing
-    'import/extensions': ['error', 'always', {
-      'js': 'never',
-      'vue': 'never'
-    }],
-    // allow optionalDependencies
-    'import/no-extraneous-dependencies': ['error', {
-      'optionalDependencies': ['test/unit/index.js']
-    }],
+    // allow paren-less arrow functions
+    'arrow-parens': 0,
+    // allow async-await
+    'generator-star-spacing': 0,
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
-    'vue/jsx-uses-vars': 2
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
 }
