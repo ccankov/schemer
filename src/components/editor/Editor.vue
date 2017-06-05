d3657e92641d0dc21b8b07645c12e3ad01520e97
brady-kimball <kimball.brady@gmail.com>
2017-06-05 16:52:09 -0700

    Render current element text

--- src/components/editor/Editor.vue
+++ src/components/editor/Editor.vue
@@ -3,9 +3,10 @@
     <section class="table-form">
       <!-- Table form component will go here -->
       <span>Forms and things</span>
+      <span>{{ currentElementName }}</span>
     </section>
     <section class="body">
-      <Paper :graph="graph"></Paper>
+      <Paper :graph="graph" v-on:send-element="receiveElement"></Paper>
       <section class="additional-info">
         <section class="statistics">
           <!-- Statistics will go here -->
@@ -30,7 +31,22 @@ export default {
   },
   data: function () {
     return {
-      graph: new joint.dia.Graph()
+      graph: new joint.dia.Graph(),
+      currentElement: null
+    }
+  },
+  computed: {
+    currentElementName: function () {
+      if (this.currentElement) {
+        return this.currentElement.attributes.attrs.text.text
+      } else {
+        return null
+      }
+    }
+  },
+  methods: {
+    receiveElement: function (element) {
+      this.currentElement = element
     }
   }
 }
