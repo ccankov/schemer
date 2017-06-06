ba85602d69dba3c6102e02b93edf2be6920fe9c1
brady-kimball <kimball.brady@gmail.com>
2017-06-06 10:42:12 -0700

    Add get route for  '/api'

--- dist/server.js
+++ dist/server.js
@@ -1,8 +1,15 @@
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
-app = express();
-app.use(serveStatic(__dirname));
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
+var express = require('express')
+var path = require('path')
+var serveStatic = require('serve-static')
+var MongoClient = require('mongodb').MongoClient
+// var url = 'mongodb://localhost/userDB'
+
+const app = express()
+app.use(serveStatic(__dirname))
+var port = process.env.PORT || 5000
+app.listen(port)
+console.log('server started ' + port)
+
+app.get('/api', (req, res) => {
+  res.send('hello world!')
+})
