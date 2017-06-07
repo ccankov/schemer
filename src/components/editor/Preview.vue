1bd339a2c04076a95fdaf20de22b3ed887d6e1a6
brady-kimball <kimball.brady@gmail.com>
2017-06-07 11:04:12 -0700

    Add preview vue component

---
+++ src/components/editor/Preview.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+  <section class="sql-preview">
+    <span>SQL Preview</span>
+    <span class="code">{{sql}}</span>
+  </section>
+</template>
+
+<script>
+import {createSQL} from '../../util/sql_util.js'
+
+export default {
+  computed: {
+    sql: function () {
+      let json = this.$store.state.graphJSON
+      return createSQL(json)
+    }
+  }
+}
+</script>
+
+<style lang="css">
+  .sql-preview {
+    display: flex;
+    flex-direction: column;
+  }
+
+  .code {
+    white-space: pre;
+    background-color:#EEEEEE;
+    font-family:Consolas,Monaco,Lucida Console,Liberation Mono,DejaVu Sans Mono,Bitstream Vera Sans Mono,Courier New;
+  }
+</style>
