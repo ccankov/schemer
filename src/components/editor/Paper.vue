18909479a6361dba6326701454b42efc2572ebc9
Nikita Shalimov <ndshalimov@gmail.com>
2017-06-08 10:02:40 -0700

    Refactor paper

--- src/components/editor/Paper.vue
+++ src/components/editor/Paper.vue
@@ -13,13 +13,14 @@ export default {
     }
   },
   mounted () {
-    // Pull graph out of props
-    const graph = this.graph
-
     // Set up paper
-    this.paper = createPaper(this.$refs.paper, graph, this)
+    // maybe change this.graph.graph
+    this.paper = createPaper(this.$refs.paper, this.graph.graph, this)
 
     // Define sample table with two columns
+    const usersTable = this.graph.addTable('users')
+
+
     const usersTable = createTable('users')
     const colId = usersTable[0].attributes.addColumn('id', 'integer', { 'primary key': true, 'allow null': false })
     const colUsername = usersTable[0].attributes.addColumn('username', 'string', { 'allow null': false })
