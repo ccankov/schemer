c556cedc721c9148fe824b4a5123957800f185e3
Nikita Shalimov <ndshalimov@gmail.com>
2017-06-08 10:22:42 -0700

    Add Cell class

--- src/util/cell.js
+++ src/util/cell.js
@@ -3,8 +3,35 @@ class Cell {
     this.element = element
   }
 
+  getName () {
+    return this.element.attributes.attrs.nodeName
+  }
 
+  setName (name) {
+    this.element.attr('text', { text: name })
+    this.element.attr('nodeName', { value: name })
+    return name
+  }
 
+  parentId () {
+    return this.element.attributes.parent
+  }
+
+  embeds () {
+    return this.element.attributes.embeds
+  }
+
+  type () {
+    return this.element.attributes.attrs.nodeType.value
+  }
+
+  isTable () {
+    return this.type === 'table'
+  }
+
+  isCol () {
+    return this.type === 'column'
+  }
 }
 
 export default Cell
