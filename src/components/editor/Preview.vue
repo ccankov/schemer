f6427e565048fb779ec0cc5e4c40ece32b08fb6f
brady-kimball <kimball.brady@gmail.com>
2017-06-09 16:26:43 -0700

    EXPORT SQL BABY

--- src/components/editor/Preview.vue
+++ src/components/editor/Preview.vue
@@ -5,18 +5,22 @@
       <pre>
         <code class="code">{{sql}}</code>
       </pre>
-      <label>
-        <span>SQL Language:</span>
-        <select v-model='sqlLang'>
-          <option v-for='lang in languages'>{{lang}}</option>
-        </select>
-      </label>
+      <section class='sql-options'>
+        <label>
+          <span>SQL Language:</span>
+          <select v-model='sqlLang'>
+            <option v-for='lang in languages'>{{lang}}</option>
+          </select>
+        </label>
+        <span class='button' @click='exportSQL'> Export SQL </span>
+      </section>
     </section>
   </section>
 </template>
 
 <script>
 import { RECEIVE_LANGUAGE } from '../../store/mutation_types'
+var FileSaver = require('file-saver')
 
 export default {
   props: ['sql'],
@@ -31,6 +35,12 @@ export default {
       set: function (newLang) {
         this.$store.commit(RECEIVE_LANGUAGE, { sqlLang: newLang })
       }
+    }
+  },
+  methods: {
+    exportSQL: function () {
+      let blob = new Blob([this.sql], {type: 'text/plain;charset=utf-8'})
+      FileSaver.saveAs(blob, `${this.$store.state.graphJSON.dbName}.sql`)
     }
   }
 }
@@ -50,7 +60,7 @@ export default {
   }
 
   pre {
-    flex: 2;
+    flex: 5;
     display: flex;
     background-color:#EEEEEE;
     padding: 3px;
@@ -65,10 +75,32 @@ export default {
     flex-direction: column;
   }
 
+  .sql-options {
+    background-color: #EEE;
+    border-left: 1px solid black;
+  }
+
   .code {
     text-align: left;
     margin: 0;
     white-space: pre;
+  }
+
+  .button {
+    display: block;
+    color: #1c56b2;
+    font-weight: 500;
+    margin: 5px 10px;
+    border: 1px solid #1c56b2;
+    border-radius: 5px;
+    padding: 3px;
+  }
+
+  .button:hover {
+    opacity: .7;
+    cursor: pointer;
+    /*text-decoration: underline;*/
+    transition: border .3s;
   }
 
   ::-webkit-scrollbar {
