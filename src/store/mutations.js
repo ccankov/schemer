b7aca518841d2bf7c9b72ac23fcf10c346dd307f
Nikita Shalimov <ndshalimov@gmail.com>
2017-06-09 09:46:49 -0700

    Add dbName and sqlLang to graph slice of state

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,18 +1,33 @@
-import { RECEIVE_GRAPH, RECEIVE_ERRORS, CLEAR_ERRORS } from './mutation_types'
+import {
+  RECEIVE_GRAPH,
+  RECEIVE_ERRORS,
+  CLEAR_ERRORS,
+  RECEIVE_DBNAME,
+  RECEIVE_LANGUAGE
+} from './mutation_types'
 
 export const state = {
-  graphJSON: { cells: [] },
+  graphJSON: {
+    cells: [],
+    dbName: 'LibaryDB',
+    sqlLang: 'postgreSQL'
+  },
   currentUser: {
     id: 1,
     username: 'brady'
   },
-  dbName: 'LibaryDB',
   errors: []
 }
 
 export const mutations = {
   [RECEIVE_GRAPH] (state, { graphJSON }) {
-    state.graphJSON = graphJSON
+    state.graphJSON.cells = graphJSON.cells
+  },
+  [RECEIVE_DBNAME] (state, { dbName }) {
+    state.graphJSON.dbName = dbName
+  },
+  [RECEIVE_LANGUAGE] (state, { sqlLang }) {
+    state.graphJSON.sqlLang = sqlLang
   },
   [RECEIVE_ERRORS] (state, { errors }) {
     state.errors = errors
