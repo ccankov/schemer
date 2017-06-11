25fb276c2e2154aa35a585095013ebfe6ac3014f
Nikita Shalimov <ndshalimov@gmail.com>
2017-06-10 21:35:20 -0700

    Add express routes to access dbs

--- dist/static/server/index.js
+++ dist/static/server/index.js
@@ -6,6 +6,7 @@ const MongoStore = require('connect-mongo')(session)
 const config = require('./config/main')
 const passport = require('./config/passport')
 const authRoutes = require('./routes/auth_routes')
+const dbRoutes = require('./routes/db_routes')
 
 // create new server
 const server = express()
@@ -25,6 +26,7 @@ server.use(express.static('public'))
 server.use(passport.initialize())
 server.use(passport.session())
 server.use('/api', authRoutes)
+server.use('/api', dbRoutes)
 server.use(serveStatic(__dirname))
 
 server.get('/', (req, res) => {
