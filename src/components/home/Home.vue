a2e42cdcdd50d68fbe16169fc9351c32dc8a191b
Nikita Shalimov <ndshalimov@gmail.com>
2017-06-11 23:58:28 -0700

    Add on home page

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,7 +1,11 @@
 <template>
   <div class='home'>
     <aside class='side-nav'>
-      nav
+      <ul class='home-links'>
+        <li @click="$router.push('/editor')"> Design DB </li>
+        <li @click="$router.push('/home/features')"> Features </li>
+        <li @click="$router.push('/home/about')"> About Us </li>
+      </ul>
     </aside>
     <router-view></router-view>
   </div>
@@ -23,13 +27,46 @@ export default {
   }
 
   .side-nav {
+    height: 100%;
     border-right: 1px solid $gray;
+    padding: 0;
     width: 20vw;
     min-width: 300px;
+    display: flex;
   }
 
   .body {
     flex: 1;
     max-width: calc(100%-300px)
+  }
+
+  .home-links {
+    padding: 0;
+    margin: 0;
+    list-style: none;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+
+    width: 100%;
+  }
+
+  .home-links li {
+    box-sizing: border-box;
+    border-bottom: 1px solid $gray;
+    width: 100%;
+    font-family: $heading;
+    font-weight: 500;
+    padding: 15px 20px;
+    font-size: 26px;
+    text-align: center;
+  }
+
+  .home-links li:hover {
+    cursor: pointer;
+    background: rgba($accent, 0.3);
+    text-decoration: underline;
   }
 </style>
